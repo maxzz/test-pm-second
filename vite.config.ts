ee621c20d51260057425e8398ce1bb951a1e8526
Max Zakharzhevskiy <maxzz@msn.com>
2021-03-30 03:34:40 -0700

    Fix: add correct production baseUrl

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
-export default (command) => {
+export default ({command}) => {
     return defineConfig({
         base: command === 'build' ? '/test-pm-second/' : '/',
         plugins: [reactRefresh()]
