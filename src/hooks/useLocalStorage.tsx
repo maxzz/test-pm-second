10c1d9bbbf86e4ee26f0b436ea7409e61a1486bb
Max Zakharzhevskiy <maxzz@msn.com>
2021-04-19 02:50:12 -0700

    WIP: add useLocalStorage clean up

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -1,28 +1,13 @@
 import { useState, useEffect } from 'react';
 
-export const Storage = {
-    get(key: string, defaultValue: any) {
-        try {
-            let str = localStorage.getItem(key);
-            return str !== null ? JSON.parse(str) : defaultValue;
-        } catch (error) {
-            return defaultValue;
-        }
-    },
-    set(key: string, value: any) {
-        localStorage.setItem(key, JSON.stringify(value));
-    },
-    remove(key: string) {
-        localStorage.removeItem(key);
-    }
-};
+// TODO: debounce, if (window && listenToStorageChanges) useEventListener(window, 'storage', read);
 
 function useLocalStorage<T>(key: string, defaultValue?: T | (() => T)) {
     let [value, setValue] = useState<T>(() => {
         let stored = localStorage.getItem(key);
         if (stored !== null) {
             try {
-                return JSON.parse(stored);    
+                return JSON.parse(stored);
             } catch (error) {
                 return defaultValue;
             }
@@ -31,7 +16,7 @@ function useLocalStorage<T>(key: string, defaultValue?: T | (() => T)) {
     });
 
     useEffect(() => {
-        if (value === null || value === undefined) {
+        if (!value) {
             localStorage.removeItem(key);
         } else {
             localStorage.setItem(key, JSON.stringify(value));
