dff92c4307189ad107e3b972a623700afe6616ad
Max Zakharzhevskiy <maxzz@msn.com>
2021-12-08 19:28:45 -0800

    WIP. bring to live. add simple user image

--- src/App.tsx
+++ src/App.tsx
@@ -38,11 +38,23 @@ function Header() {
     );
 }
 
+
+export function PhUserCircleDuotone(props: React.SVGProps<SVGSVGElement>) {
+    return (
+        <svg width="1em" height="1em" viewBox="0 0 256 256" fill="currentColor" {...props}>
+            <path opacity=".2" d="M128 32a96 96 0 0 0-64.201 167.375v-.001A71.997 71.997 0 0 1 128 160a40 40 0 1 1 40-40a40 40 0 0 1-40 40a71.997 71.997 0 0 1 64.201 39.374A96 96 0 0 0 128 32z" />
+            <path d="M232 128a104 104 0 1 0-174.18 76.681a7.962 7.962 0 0 0 1.326 1.19a103.784 103.784 0 0 0 137.712-.004a7.954 7.954 0 0 0 1.317-1.181A103.747 103.747 0 0 0 232 128zm-192 0a88 88 0 1 1 153.758 58.41a79.56 79.56 0 0 0-36.056-28.745a48 48 0 1 0-59.404 0a79.56 79.56 0 0 0-36.056 28.745A87.638 87.638 0 0 1 40 128zm56-8a32 32 0 1 1 32 32a32.036 32.036 0 0 1-32-32zm-21.917 77.499a64.022 64.022 0 0 1 107.834 0a87.814 87.814 0 0 1-107.834 0z" />
+        </svg>
+    );
+}
+
 function LoginForm({ setShowBabba }: { setShowBabba: React.Dispatch<React.SetStateAction<boolean>>; }) {
     const [username, setUsername] = useLocalStorage('pm-test-2-username', 'maxzz');
     const [password, setPassword] = useLocalStorage('pm-test-2-password', '123456');
     return (
         <form id="test" className="pt-8 pb-4">
+            <PhUserCircleDuotone />
+
             {/* Username */}
             <div className="text-gray-600">Username</div>
             <input className="inp" spellCheck="false" autoComplete="email" value={username} onChange={e => setUsername(e.target.value)} />
