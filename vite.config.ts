dcf47df43ea0fb51e9a3fb243f0a500e4e7dd255
Max Zakharzhevskiy <maxzz@msn.com>
2021-12-12 00:40:14 -0800

    Add sizes visualization and SVG inlining

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,26 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import { visualizer } from 'rollup-plugin-visualizer';
+import url from '@rollup/plugin-url';
 
-// https://vitejs.dev/config/
-export default ({command}) => {
+export default ({ command }) => {
     return defineConfig({
         base: command === 'build' ? '/test-pm-second/' : '/',
-        plugins: [reactRefresh()]
+        plugins: [
+            reactRefresh(),
+            {
+                ...url({
+                    include: ['**/*.svg'],
+                    limit: 15000,
+                }),
+                enforce: 'pre',
+            },
+            visualizer({
+                filename: 'vizualization.html',
+                template: 'sunburst', // sunburst - d3 style (good as default as well); treemap - table (default); network - graph (slow to open).
+                gzipSize: true,
+                brotliSize: true,
+            }),
+        ]
     });
-}
-
+};
