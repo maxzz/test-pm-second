498914a02b8082eab8f86a1aff157e089e009335
Max Zakharzhevskiy <maxzz@msn.com>
2022-05-19 18:00:19 -0700

    WIP. AppHeader. update Ghost aniamtions

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { IconGhost } from "./UI/Icons";
-import { a, useSpring } from "@react-spring/web";
+import { a, config, useSpring } from "@react-spring/web";
 
 const AIconGhost = a(IconGhost);
 
 function Ghost() {
-    const color = '#e0e7ff80';
+    const color = '#e0e7ff80'; //text-indigo-100/50
     const styles = useSpring({
         from: {
             x: 1000,
@@ -14,14 +14,14 @@ function Ghost() {
             color,
         },
         to: [
-            { opacity: 1, x: 0 },
-            { opacity: 1, x: -400 },
-            { opacity: 1, x: 0 },
-            { scale: 2, color: 'white', config: { duration: 1000, }, },
+            { opacity: 1, x: 0, config: config.default, },
+            { opacity: 1, x: -400, config: config.default, },
+            { opacity: 1, x: 0, config: config.default, },
+            { scale: 2, color: 'white', config: { duration: 700, }, },
             { scale: 1 },
-            { scale: 2, color: 'white', config: { duration: 1000, }, },
+            { scale: 2, color: 'white', config: { duration: 400, }, },
             { scale: 1, color, x: -50 },
-            { opacity: 0, scale: 0, config: { duration: 700, }, },
+            { opacity: 0, scale: 0, config: config.default, },
         ],
         config: { duration: 600, },
     });
@@ -35,7 +35,7 @@ const textShadow = { textShadow: '2px 3px #735bbf' };
 
 export function AppHeader() {
     return (
-        <header className="p-4 bg-indigo-900 flex items-center space-x-4" style={headerShadow}>
+        <header className="p-4 bg-indigo-900 flex items-center space-x-4 overflow-hidden" style={headerShadow}>
             <div className="text-2xl font-thin text-white" style={textShadow}>
                 Password Manager Another Test
             </div>
