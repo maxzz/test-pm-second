5d7fae147256ac2bf2adaa882c584da287c054c7
Max Zakharzhevskiy <maxzz@msn.com>
2022-05-20 16:34:49 -0700

    WIP. GhostMain. ghost multi-step animation. react-spring does not like .5 numbers

--- src/components/GhostMain.tsx
+++ src/components/GhostMain.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAtom } from 'jotai';
 import { showBabbaAtom } from '../store/store';
 import { IconGhost } from './UI/Icons';
@@ -11,55 +11,90 @@ export function GhostMain() {
 
     const [open, setOpen] = React.useState(false);
 
-    // const [show, setShowBabba] = useState(false);
-    // const [styles, api] = useSpring(() => ({
-    //     from: { opacity: 0, scaleX: 1, },
-    //     to: [
-    //         { opacity: 1, scaleX: -1, },
-    //         { opacity: 1, scaleX: 1, },
-    //         { opacity: 1, scaleX: -1, },
-    //     ],
-    //     reset: open,
-    //     config: { easings: easings.easeOutBounce, duration: 200, },
-    //     //
-    // }));
-    // React.useEffect(() => setShowBabba(true));
+    /*NO* /
+    const [styles, api] = useSpring(() => ({
+        from: { opacity: 0, scaleX: 1, },
+        to: [
+            { opacity: 1, scaleX: -1, },
+            { opacity: 1, scaleX: 1, },
+            { opacity: 1, scaleX: -1, },
+        ],
+        reset: open,
+        config: { easings: easings.easeOutBounce, duration: 200, },
+    }));
+    /**/
 
-    // const styles = useSpring({
-    //     from: { opacity: 0, scale: [1, 0.5], },
-    //     to: [
-    //         { opacity: 1, scale: [2.5, 0.5], },
-    //         { opacity: 1, scale: [.3, 1.3], },
-    //         { opacity: 1, scale: [1.5, 0.5], },
-    //         { opacity: 1, scale: [.3, 1], },
-    //         { opacity: 1, scale: [1.5, .5], },
-    //         { opacity: 1, scale: [.2, .5], config: { easings: easings.easeOutBounce, duration: 600, }, },
-    //         { opacity: 1, scale: [-.2, .5], config: { easings: easings.easeOutBounce, duration: 100, }, },
-    //         { opacity: 1, scale: [-1, 1], config: { easings: easings.easeOutBounce, duration: 600, }, },
-    //     ],
-    //     reset: open,
-    //     //config: { easings: easings.easeOutBounce, duration: 200, },
-    //     //
-    // });
+    /*OK* /
+    const styles = useSpring({
+        from: { opacity: 0, scale: [1, 0.5], },
+        to: [
+            { opacity: 1, scale: [2.5, 0.5], },
+            { opacity: 1, scale: [0.3, 1.3], },
+            { opacity: 1, scale: [1.5, 0.5], },
+            { opacity: 1, scale: [0.3, 1], },
+            { opacity: 1, scale: [1.5, 0.5], },
+            { opacity: 1, scale: [0.2, 0.5], config: { easings: easings.easeOutBounce, duration: 600, }, },
+            { opacity: 1, scale: [-0.2, 0.5], config: { easings: easings.easeOutBounce, duration: 100, }, },
+            { opacity: 1, scale: [-1, 1], config: { easings: easings.easeOutBounce, duration: 600, }, },
+        ],
+        reset: open,
+        //config: { easings: easings.easeOutBounce, duration: 200, },
+    });
+    /**/
 
+    /*OK* /
+    const [styles, api] = useSpring({
+        //ref: springRef,
+        from: { opacity: 0, x: 0, y: 0, scale: [1, 1] },
+        to: [
+            { opacity: 1, scale: [0.2, 0.2], },
+            { opacity: 1, scale: [1.5, 1], },
+            { opacity: 1, scale: [0.3, 1.3], },
+            { opacity: 1, scale: [1.5, 0.5], },
+            { opacity: 1, scale: [0.3, 1], },
+            { opacity: 1, scale: [1.5, 0.5], },
+            { x: 100, y: 500, },
+            { opacity: 1, scale: [0.2, 0.5], config: { easings: easings.easeOutBounce, duration: 600, }, },
+            { opacity: 1, scale: [-0.2, 0.5], config: { easings: easings.easeOutBounce, duration: 100, }, },
+            { opacity: 1, scale: [-1, 1], config: { easings: easings.easeOutBounce, duration: 600, }, },
+            { opacity: 0, x: 0, y: 0, },
+        ],
+        reset: true,
+        //config: { easings: easings.easeOutBounce, duration: 200, },
+    }, [open]);
+    /**/
+
+    /*OK* /
     //const springRef = useSpringRef();
     const [styles, api] = useSpring({
         //ref: springRef,
-        from: { opacity: 0, scale: [1, 0.5], },
+        from: { opacity: 0, x: 0, y: 0, scale: '1, 1' },
         to: [
-            { opacity: 1, scale: [1.5, 0.5], },
-            { opacity: 1, scale: [.3, 1.3], },
-            { opacity: 1, scale: [1.5, 0.5], },
-            { opacity: 1, scale: [.3, 1], },
-            { opacity: 1, scale: [1.5, .5], },
-            { opacity: 1, scale: [.2, .5], config: { easings: easings.easeOutBounce, duration: 600, }, },
-            { opacity: 1, scale: [-.2, .5], config: { easings: easings.easeOutBounce, duration: 100, }, },
-            { opacity: 1, scale: [-1, 1], config: { easings: easings.easeOutBounce, duration: 600, }, },
-],
+            { x: 0, y: 0, },
+            { opacity: 1, scale: '0.5, 1', config: { easings: easings.easeOutBounce, duration: 2200, }, },
+            { scale: '0.2, 0.5', config: { easings: easings.easeOutBounce, duration: 2200, }, },
+            { x: 100, y: 500, },
+        ],
         reset: true,
-        //config: { easings: easings.easeOutBounce, duration: 200, },
-        //
+        config: { easings: easings.easeOutBounce, duration: 1000, },
     }, [open]);
+    /**/
+
+    /*OK*/
+    const springRef = useSpringRef();
+    const styles = useSpring({
+        ref: springRef,
+        from: { opacity: 0, x: 0, y: 0, scale: '1, 1' },
+        to: [
+            { opacity: 1, scale: '0.5, 1', config: { easings: easings.easeOutBounce, duration: 2200, }, },
+            { scale: '0.2, 0.5', config: { easings: easings.easeOutBounce, duration: 2200, }, },
+            { x: 100, y: 500, },
+        ],
+        reset: true,
+        config: { easings: easings.easeOutBounce, duration: 1000, },
+    });
+    useEffect(() => { springRef.start(); }, [open]);
+    /**/
 
     return (
         <div>
