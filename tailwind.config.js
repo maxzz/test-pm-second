292f4f7579071109b7654cfca5fbffc5e061fea6
Max Zakharzhevskiy <maxzz@msn.com>
2022-05-22 02:32:14 -0700

    WIP. bug. vitejs does not handle images from tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
     theme: {
         extend: {
             backgroundImage: {
-                'hero-pattern': 'url("./src/assets/svg-bkg-turbulance.svg")'
+                //'hero-pattern': 'url("/src/assets/svg-bkg-turbulance.svg")'
+                'hero-pattern': `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='500' height='500'%3E%3Cfilter id='n'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='.9' numOctaves='10' stitchTiles='stitch'%3E%3C/feTurbulence%3E%3C/filter%3E%3Crect width='500' height='500' fill='%2311223301'%3E%3C/rect%3E%3Crect width='500' height='500' filter='url(%23n)' opacity='0.4'%3E%3C/rect%3E%3C/svg%3E")`
             }
         },
     },
@@ -13,3 +14,17 @@ module.exports = {
     },
     plugins: [],
 };
+
+/*
+const bkg = require("./src/assets/svg-bkg-turbulance.svg");
+    ...
+    theme: {
+        extend: {
+            backgroundImage: {
+                'hero-pattern': `url(${bkg})`
+            }
+        },
+    },
+    ...
+};
+*/
