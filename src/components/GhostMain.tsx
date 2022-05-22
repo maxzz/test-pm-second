98fa9934c4924ce11ea94e3cf66773d5593c9525
Max Zakharzhevskiy <maxzz@msn.com>
2022-05-21 17:35:37 -0700

    WIP. GhostMain. tweak animation interpolation. 4. experimants major code clean up

--- src/components/GhostMain.tsx
+++ src/components/GhostMain.tsx
@@ -6,98 +6,10 @@ import { a, easings, useSpring } from '@react-spring/web';
 
 const AIconGhost = a(IconGhost);
 
-// 1. Don't use short number notation like .5 use only 0.5.
-// 2. animated param should be defined in 'from' before appearing into 'to'.
-
 export function GhostMain() {
     // const [show, setShowBabba] = useAtom(showBabbaAtom2);
 
     const [open, setOpen] = React.useState(false);
-
-    /*NO* /
-    const [styles, api] = useSpring(() => ({
-        from: { opacity: 0, scaleX: 1, },
-        to: [
-            { opacity: 1, scaleX: -1, },
-            { opacity: 1, scaleX: 1, },
-            { opacity: 1, scaleX: -1, },
-        ],
-        reset: open,
-        config: { easings: easings.easeOutBounce, duration: 200, },
-    }));
-    /**/
-
-    /*OK* /
-    const styles = useSpring({
-        from: { opacity: 0, scale: [1, 0.5], },
-        to: [
-            { opacity: 1, scale: [2.5, 0.5], },
-            { opacity: 1, scale: [0.3, 1.3], },
-            { opacity: 1, scale: [1.5, 0.5], },
-            { opacity: 1, scale: [0.3, 1], },
-            { opacity: 1, scale: [1.5, 0.5], },
-            { opacity: 1, scale: [0.2, 0.5], config: { easings: easings.easeOutBounce, duration: 600, }, },
-            { opacity: 1, scale: [-0.2, 0.5], config: { easings: easings.easeOutBounce, duration: 100, }, },
-            { opacity: 1, scale: [-1, 1], config: { easings: easings.easeOutBounce, duration: 600, }, },
-        ],
-        reset: open,
-        //config: { easings: easings.easeOutBounce, duration: 200, },
-    });
-    /**/
-
-    /*OK* /
-    const [styles, api] = useSpring({
-        //ref: springRef,
-        from: { opacity: 0, x: 0, y: 0, scale: [1, 1] },
-        to: [
-            { opacity: 1, scale: [0.2, 0.2], },
-            { opacity: 1, scale: [1.5, 1], },
-            { opacity: 1, scale: [0.3, 1.3], },
-            { opacity: 1, scale: [1.5, 0.5], },
-            { opacity: 1, scale: [0.3, 1], },
-            { opacity: 1, scale: [1.5, 0.5], },
-            { x: 100, y: 500, },
-            { opacity: 1, scale: [0.2, 0.5], config: { easings: easings.easeOutBounce, duration: 600, }, },
-            { opacity: 1, scale: [-0.2, 0.5], config: { easings: easings.easeOutBounce, duration: 100, }, },
-            { opacity: 1, scale: [-1, 1], config: { easings: easings.easeOutBounce, duration: 600, }, },
-            { opacity: 0, x: 0, y: 0, },
-        ],
-        reset: true,
-        //config: { easings: easings.easeOutBounce, duration: 200, },
-    }, [open]);
-    /**/
-
-    /*OK* /
-    //const springRef = useSpringRef();
-    const [styles, api] = useSpring({
-        //ref: springRef,
-        from: { opacity: 0, x: 0, y: 0, scale: '1, 1' },
-        to: [
-            { x: 0, y: 0, },
-            { opacity: 1, scale: '0.5, 1', config: { easings: easings.easeOutBounce, duration: 2200, }, },
-            { scale: '0.2, 0.5', config: { easings: easings.easeOutBounce, duration: 2200, }, },
-            { x: 100, y: 500, },
-        ],
-        reset: true,
-        config: { easings: easings.easeOutBounce, duration: 1000, },
-    }, [open]);
-    /**/
-
-    /*OK* /
-    const springRef = useSpringRef();
-    const styles = useSpring({
-        ref: springRef,
-        from: { opacity: 0, x: 0, y: 0, scale: '1, 1' },
-        to: [
-            { opacity: 1, scale: '0.5, 1', config: { easings: easings.easeOutBounce, duration: 2200, }, },
-            { scale: '0.2, 0.5', config: { easings: easings.easeOutBounce, duration: 2200, }, },
-            { x: 100, y: 500, },
-        ],
-        reset: true,
-        config: { easings: easings.easeOutBounce, duration: 1000, },
-    });
-    useEffect(() => { springRef.start(); }, [open]);
-    /**/
 
     const [styles, api] = useSpring({
         //from: { opacity: 1, scale: '1, 1', n: 0, fill: '#312e8180', },
