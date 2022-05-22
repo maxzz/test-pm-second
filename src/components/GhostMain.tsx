40a3980cad64d11b148fc3668eaf8d2bb86303b2
Max Zakharzhevskiy <maxzz@msn.com>
2022-05-21 18:19:36 -0700

    WIP. GhostMain. tweak animation interpolation. 6. move calc from render stuff

--- src/components/GhostMain.tsx
+++ src/components/GhostMain.tsx
@@ -6,19 +6,28 @@ import { a, easings, useSpring, useSpringRef } from '@react-spring/web';
 
 const AIconGhost = a(IconGhost);
 
+const animationProps = {
+    from: { opacity: .25, scale: '0.5, 0.5', n: 0, fill: '#312e81', },
+    to: [
+        { n: 1, opacity: 1, config: { duration: 900, }, }, // #312e81 to have it flat
+        { scale: '0.1, 1', config: { duration: 300, }, },
+        { scale: '1, 0.5', config: { duration: 600, }, },
+        { scale: '0.7, 1', config: { duration: 600, }, },
+        { scale: '1.1, 0.9', config: { duration: 300, }, },
+        { scale: '0.9, 1', config: { duration: 600, }, },
+        { scale: '0, 0', },
+        { scale: '-1, 1', opacity: 1, fill: '#ff0000', },
+    ]
+};
+
+const xPos = (pos: number, max: number) => ({ range: [0, 0.25, 0.5, 0.75, 1], output: [100, max * 0.7, max * 0.1, max * 0.5, pos + 8] });
+const yPos = (pos: number, max: number) => ({ range: [0, 0.5, 1], output: [100, pos / 2, pos] });
+
 export function GhostMain() {
-    // const [show, setShowBabba] = useAtom(showBabbaAtom2);
-
-    const [open, setOpen] = React.useState(false);
-
     const api = useSpringRef();
     const styles = useSpring({
-        //from: { opacity: 1, scale: '1, 1', n: 0, fill: '#312e8180', },
         ref: api,
-        //from: { n: 0, opacity: 0 },
         from: { opacity: 0, scale: '0.5, 0.5', n: 0, fill: '#312e81', },
-        //reset: true,
-        //config: { easings: easings.easeOutBounce, duration: 400, },
     });
 
     const { n, ...rest } = styles;
@@ -30,7 +39,7 @@ export function GhostMain() {
     pos.x -= 36; // Left edge of ghost SVG.
     pos.y -= 94; // App header height (64) and top edge of ghost SVG (30).
 
-    //console.log('ghostTarget', ghostTarget ? {x: pos.x, y: pos.y} : '-------------null', {wArea, hArea});
+    console.log('ghostTarget', ghostTarget ? {x: pos.x, y: pos.y} : '-------------null', {wArea, hArea});
 
     return (
         <div>
@@ -39,32 +48,21 @@ export function GhostMain() {
                 type="button"
                 value="Reload"
                 onClick={() => {
-                    setOpen((v) => !v);
-                    //console.log('click', open);
-                    api.start({
-                        from: { opacity: .25, scale: '0.5, 0.5', n: 0, fill: '#312e81', },
-                        to: [
-                            { n: 1, opacity: 1, config: { duration: 900, }, }, // #312e81 to have it flat
-                            { scale: '0.1, 1', config: { duration: 300, }, },
-                            { scale: '1, 0.5', config: { duration: 600, }, },
-                            { scale: '0.7, 1', config: { duration: 600, }, },
-                            { scale: '1.1, 0.9', config: { duration: 300, }, },
-                            { scale: '0.9, 1', config: { duration: 600, }, },
-                            { scale: '0, 0', },
-                            { scale: '-1, 1', opacity: 1, fill: '#ff0000', },
-                        ]
-                    }
-                    );
+                    api.start(animationProps);
                 }}
             />
 
             {/* <AIconGhost style={styles} className="absolute left-0 top-0 w-32 h-32 text-indigo-900" strokeWidth={.7} /> */}
 
-            <AIconGhost data-n={n} style={{
-                x: n.to({ range: [0, 0.25, 0.5, 0.75, 1], output: [100, wArea * 0.7, wArea * 0.1, wArea * 0.5, pos.x + 8] }),
-                y: n.to({ range: [0, 0.5, 1], output: [100, pos.y / 2, pos.y] }),
-                ...rest,
-            }} className="absolute left-0 top-0 w-32 h-32 fill-slate-500 text-indigo-900" strokeWidth={.7} />
+            <AIconGhost
+                data-n={n}
+                style={{
+                    x: n.to(xPos(pos.x, wArea)),
+                    y: n.to(yPos(pos.y, hArea)),
+                    ...rest,
+                }}
+                className="absolute left-0 top-0 w-32 h-32 fill-slate-500 text-indigo-900" strokeWidth={.7}
+            />
         </div>
     );
 }
