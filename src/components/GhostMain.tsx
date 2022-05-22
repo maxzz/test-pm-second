7d48595be0ee9110f43e90f81ea3a740a78d9e57
Max Zakharzhevskiy <maxzz@msn.com>
2022-05-21 18:29:52 -0700

    WIP. GhostMain. tweak animation interpolation. 7. fix ghost location

--- src/components/GhostMain.tsx
+++ src/components/GhostMain.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAtomValue } from 'jotai';
 import { ghostTargetAtom, workingAreaAtom } from '../store/store';
 import { IconGhost } from './UI/Icons';
@@ -12,9 +12,9 @@ const animationProps = {
         { n: 1, opacity: 1, config: { duration: 900, }, }, // #312e81 to have it flat
         { scale: '0.1, 1', config: { duration: 300, }, },
         { scale: '1, 0.5', config: { duration: 600, }, },
-        { scale: '0.7, 1', config: { duration: 600, }, },
-        { scale: '1.1, 0.9', config: { duration: 300, }, },
-        { scale: '0.9, 1', config: { duration: 600, }, },
+        { scale: '0.7, 1', config: { duration: 400, }, },
+        { scale: '1.1, 0.9', config: { duration: 600, }, },
+        { scale: '0.9, 1', config: { duration: 400, }, },
         { scale: '0, 0', },
         { scale: '-1, 1', opacity: 1, fill: '#ff0000', },
     ]
@@ -23,7 +23,18 @@ const animationProps = {
 const xPos = (pos: number, max: number) => ({ range: [0, 0.25, 0.5, 0.75, 1], output: [100, max * 0.7, max * 0.1, max * 0.5, pos + 8] });
 const yPos = (pos: number, max: number) => ({ range: [0, 0.5, 1], output: [100, pos / 2, pos] });
 
+const getTargetPos = (target: HTMLDivElement | null, def: { x: number; y: number; }) => {
+    const pos = target?.getBoundingClientRect() || def;
+    pos.x -= 36; // Left edge of ghost SVG.
+    pos.y -= 94; // App header height (64) and top edge of ghost SVG (30).
+    return pos;
+};
+
 export function GhostMain() {
+    const ghostTarget = useAtomValue(ghostTargetAtom);
+    const { width: wArea, height: hArea } = useAtomValue(workingAreaAtom);
+    const [pos, setPos] = useState({ x: 0, y: 0 });
+
     const api = useSpringRef();
     const styles = useSpring({
         ref: api,
@@ -31,16 +42,6 @@ export function GhostMain() {
     });
 
     const { n, ...rest } = styles;
-
-    const { width: wArea, height: hArea } = useAtomValue(workingAreaAtom);
-    const ghostTarget = useAtomValue(ghostTargetAtom);
-
-    const pos = ghostTarget?.getBoundingClientRect() || { x: wArea / 2, y: hArea / 2 };
-    pos.x -= 36; // Left edge of ghost SVG.
-    pos.y -= 94; // App header height (64) and top edge of ghost SVG (30).
-
-    console.log('ghostTarget', ghostTarget ? {x: pos.x, y: pos.y} : '-------------null', {wArea, hArea});
-
     return (
         <div>
             <input
@@ -48,6 +49,7 @@ export function GhostMain() {
                 type="button"
                 value="Reload"
                 onClick={() => {
+                    setPos(getTargetPos(ghostTarget, { x: wArea / 2, y: hArea / 2 }));
                     api.start(animationProps);
                 }}
             />
