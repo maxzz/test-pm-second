8001da1f04c95f77ac0519aa289e3093fd00b235
Max Zakharzhevskiy <maxzz@msn.com>
2024-08-03 12:57:33 -0700

    WIP. update 2024. ReloadButton code review

--- src/components/GhostBabbaWLengthCalc.tsx
+++ src/components/GhostBabbaWLengthCalc.tsx
@@ -5,15 +5,17 @@ export function GhostBabbaWLengthCalc() {
     const pathRef = useRef<SVGPathElement>(null);
     const [pathLen, setPathLen] = useState(0);
 
-    useEffect(() => {
-        if (pathRef.current) {
-            setPathLen(pathRef.current.getTotalLength()); //console.log({pathLen: pathLen});
-        }
-    }, [pathRef.current]);
+    useEffect(
+        () => {
+            if (pathRef.current) {
+                setPathLen(pathRef.current.getTotalLength()); //console.log({pathLen: pathLen});
+            }
+        }, [pathRef.current]
+    );
 
     return (
         <div className="relative z-10">
-            <div className="absolute top-4 right-64 w-32 h-32 text-indigo-900">
+            <div className="absolute top-4 right-64 size-32 text-indigo-900">
                 <IconGhostBubba ref={pathRef} className="transform scale-x-[-1] fill-[red]" />
             </div>
         </div>
--- src/ui/ReloadButton.tsx
+++ src/ui/ReloadButton.tsx
@@ -1,16 +1,23 @@
-import React, { HTMLAttributes } from "react";
+import { HTMLAttributes } from "react";
 import { useAtom } from "jotai";
 import { loginStartedAtom } from "../store/store";
 import { classNames } from "../utils/classnames";
 
+const buttonClasses = "self-center \
+mr-2 px-2 py-1 text-xs \
+\
+text-yellow-100 \
+border-yellow-100 hover:bg-slate-900/50 border \
+rounded \
+cursor-pointer \
+active:scale-[.97]";
+
 export function ReloadButton({ className }: HTMLAttributes<HTMLInputElement>) {
     const [loginStarted, setLoginStarted] = useAtom(loginStartedAtom);
+
     return (
         <input
-            className={classNames(
-                "self-center mr-2 px-2 py-1 text-xs text-yellow-100 border-yellow-100 hover:bg-slate-900/50 border rounded active:scale-[.97] cursor-pointer",
-                className,
-            )}
+            className={classNames(buttonClasses, className,)}
             type="button"
             value={loginStarted ? 'Stop' : 'Reload'}
             onClick={() => setLoginStarted((v) => !v)}
