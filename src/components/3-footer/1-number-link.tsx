a37110eea8c470b3e6fb8bce3eafc00f67449069
Max Zakharzhevskiy <maxzz@msn.com>
2024-08-05 12:56:23 -0700

    WIP. update 2024. NumberLink code review

--- src/components/3-footer/1-number-link.tsx
+++ src/components/3-footer/1-number-link.tsx
@@ -1,16 +1,16 @@
 import { classNames } from "@/utils/classnames";
 import { AnchorHTMLAttributes } from "react";
 
+const numLinkClasses = "\
+px-2 mt-1 size-6 text-xs \
+hover:text-indigo-200 hover:bg-indigo-700 hover:scale-[1.2] transition-transform \
+border-current border rounded \
+grid place-items-center \
+";
+
 export function NumberLink({ label, className, ...rest }: { label: string; } & AnchorHTMLAttributes<HTMLAnchorElement>) {
     return (
-        <a
-            className={classNames(
-                "px-2 mt-1 w-6 h-6 text-xs border-current border rounded grid place-items-center",
-                "hover:text-indigo-200 hover:bg-indigo-700 hover:scale-[1.2] transition-transform",
-                className
-            )}
-            {...rest}
-        >
+        <a className={classNames(numLinkClasses, className)} {...rest}>
             {label}
         </a>
     );
