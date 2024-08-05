96106756e724829b2c19c82e35c0f318360e0c1a
Max Zakharzhevskiy <maxzz@msn.com>
2024-08-04 19:48:43 -0700

    WIP. update 2024. update AppSection1_Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { AppSection1_Header } from './components/AppSection1_Header';
+import { App_Header } from './components/1-header';
 import { AppSection2_Main } from './components/AppSection2_Main';
 import { AppSection3_Footer } from './components/AppSection3_Footer';
 
 export function App() {
     return (
         <div className="h-screen bg-indigo-900 bg-hero-pattern flex flex-col">
-            <AppSection1_Header />
+            <App_Header />
             <AppSection2_Main className="flex-1" />
             <AppSection3_Footer />
         </div>
--- src/components/1-header/index.tsx
+++ src/components/1-header/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { IconGhost } from "../ui/icons";
+import { IconGhost } from "../../ui/icons";
 import { a, config, useSpring } from "@react-spring/web";
 
 const AIconGhost = a(IconGhost);
@@ -33,7 +33,7 @@ function Ghost() {
 const headerShadow = { boxShadow: '2px 2px 2px rgba(0, 0, 0, 0.2)' };
 const textShadow = { textShadow: '2px 3px #1f1e58' };
 
-export function AppSection1_Header() {
+export function App_Header() {
     return (
         // <header className="p-4 bg-[#03014d] bg-hero-pattern flex items-center space-x-4 overflow-hidden" style={headerShadow}>
         <header className="p-4 bg-indigo-900 flex items-center space-x-4 overflow-hidden" style={headerShadow}>
