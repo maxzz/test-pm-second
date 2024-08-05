985d03d73692ec1b7c8780d4628a497929df8f61
Max Zakharzhevskiy <maxzz@msn.com>
2024-08-05 13:46:38 -0700

    WIP. update 2024. update LoginFrame animation

--- src/components/2-main/1-login-screen/3-login-frame.tsx
+++ src/components/2-main/1-login-screen/3-login-frame.tsx
@@ -1,19 +1,30 @@
 import { useSpring, config, a } from "@react-spring/web";
-import { formStyles } from "./0-all";
 import { LoginForm } from "./1-login-form";
 
+const formStyles = {
+    '--tw-ring-offset-color': '#fff',
+    boxShadow: 'var(--tw-ring-offset-shadow), var(--tw-ring-shadow), var(--tw-shadow, 0 0 #0000)',
+};
+
+const sectionClasses = "bg-indigo-200 rounded-lg border shadow-sm ring-2 ring-indigo-500 ring-offset-1 ring-offset-indigo-600";
+
 export function LoginFrame() {
-    const [styles, api] = useSpring(() => ({
-        from: { scaleY: .7, },
-        to: { scaleY: 1, },
-        config: { ...config.wobbly },
-    }));
+
+    const [ani, api] = useSpring(
+        () => ({
+            from: { scale: .2, },
+            to: { scale: 1, },
+            config: { ...config.wobbly },
+        })
+    );
 
     return (
-        <a.div style={{ ...styles, ...{ boxShadow: '#fff1ce4a 0px 0px 15px 6px' } }}>
-            <section className="bg-indigo-200 rounded-lg border shadow-sm ring-2 ring-indigo-500 ring-offset-1 ring-offset-indigo-600" style={formStyles}>
+        <a.div style={{ ...ani, ...{ boxShadow: '#fff1ce4a 0px 0px 15px 6px' } }}>
+
+            <section className={sectionClasses} style={formStyles}>
                 <LoginForm />
             </section>
+
         </a.div>
     );
 }
