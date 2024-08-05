8589c4887e8cbe0af2796651d9ab92c7a9a9d403
Max Zakharzhevskiy <maxzz@msn.com>
2024-08-05 13:04:34 -0700

    WIP. update 2024. GhostMain code review

--- src/components/GhostMain.tsx
+++ src/components/GhostMain.tsx
@@ -5,10 +5,22 @@ import { IconGhost } from '../ui/icons';
 import { a, easings, useSpring, useSpringRef } from '@react-spring/web';
 import { ReloadButton } from '../ui/ReloadButton';
 
-const AIconGhost = a(IconGhost);
+const GhostAnimatedIcon = a(IconGhost);
+
+function getTargetPos(target: HTMLDivElement | null, def: { x: number; y: number; }) {
+    const pos = target?.getBoundingClientRect() || def;
+    pos.x -= 36; // Left edge of ghost SVG.
+    pos.y -= 94; // App header height (64) and top edge of ghost SVG (30).
+    return pos;
+}
 
 const animationProps = {
-    from: { opacity: 0, scale: '0.5, 0.5', n: 0, fill: '#312e81', },
+    from: {
+        opacity: 0,
+        scale: '0.5, 0.5',
+        n: 0,
+        fill: '#312e81',
+    },
     to: [
         { n: 1, opacity: 1, config: { duration: 0, }, }, // skip all move transitions to test the rest of animation
         { n: 1, opacity: 1, config: { duration: 900, }, }, // #312e81 to have it flat
@@ -27,15 +39,15 @@ const animationProps = {
     ]
 };
 
-const xPos = (pos: number, max: number) => ({ range: [0, 0.25, 0.5, 0.75, 1], output: [100, max * 0.7, max * 0.1, max * 0.5, pos + 8] });
-const yPos = (pos: number, max: number) => ({ range: [0, 0.5, 1], output: [100, pos / 2, pos] });
+const xPos = (pos: number, max: number) => ({
+    range: [0, 0.25, 0.5, 0.75, 1],
+    output: [100, max * 0.7, max * 0.1, max * 0.5, pos + 8],
+});
 
-const getTargetPos = (target: HTMLDivElement | null, def: { x: number; y: number; }) => {
-    const pos = target?.getBoundingClientRect() || def;
-    pos.x -= 36; // Left edge of ghost SVG.
-    pos.y -= 94; // App header height (64) and top edge of ghost SVG (30).
-    return pos;
-};
+const yPos = (pos: number, max: number) => ({
+    range: [0, 0.5, 1],
+    output: [100, pos / 2, pos],
+});
 
 export function GhostMain() {
     const ghostTarget = useAtomValue(ghostTargetAtom);
@@ -43,29 +55,35 @@ export function GhostMain() {
     const [pos, setPos] = useState({ x: 0, y: 0 });
 
     const api = useSpringRef();
-    const styles = useSpring({ ref: api, from: animationProps.from });
-    const { n, ...rest } = styles;
+    const ani = useSpring({ ref: api, from: animationProps.from });
+    const { n, ...rest } = ani;
 
     const [loginStarted, setLoginStarted] = useAtom(loginStartedAtom);
 
-    React.useEffect(() => {
-        if (loginStarted) {
-            api.stop(true); // if we started again by reseting loginStarted, i.e. loginStarted -> false -> true
-            api.set(animationProps.from);
-            start();
-        }
-    }, [loginStarted]);
+    React.useEffect(
+        () => {
+            if (loginStarted) {
+                api.stop(true); // if we started again by reseting loginStarted, i.e. loginStarted -> false -> true
+                api.set(animationProps.from);
+                start();
+            }
+        }, [loginStarted]
+    );
 
     function start() {
         setPos(getTargetPos(ghostTarget, { x: wArea / 2, y: hArea / 2 }));
-        api.start({ ...animationProps, onRest: ({ finished }) => finished && setLoginStarted(false) });
+        api.start({
+            ...animationProps,
+            onRest: ({ finished }) => finished && setLoginStarted(false),
+        });
     }
 
     return (
         <div>
             {/* <ReloadButton /> */}
-            {loginStarted &&
-                <AIconGhost
+
+            {loginStarted && (
+                <GhostAnimatedIcon
                     data-n={n}
                     style={{
                         x: n.to(xPos(pos.x, wArea)),
@@ -74,7 +92,7 @@ export function GhostMain() {
                     }}
                     className="absolute left-0 top-0 w-32 h-32 fill-slate-500 text-indigo-900" strokeWidth={.7}
                 />
-            }
+            )}
         </div>
     );
 }
