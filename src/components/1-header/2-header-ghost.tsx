4896592ae9acfb7b7356cb3d722cfe1d91b57514
Max Zakharzhevskiy <maxzz@msn.com>
2024-08-07 13:00:39 -0700

    WIP. update 2024. update HeaderGhost

---
+++ src/components/1-header/2-header-ghost.tsx
@@ -0,0 +1,31 @@
+import { IconGhost } from "@/ui/icons";
+import { a, useSpring, config } from "@react-spring/web";
+
+const AIconGhost = a(IconGhost);
+const color = '#e0e7ff80'; //text-indigo-100/50
+
+export function HeaderGhost() {
+    const styles = useSpring({
+        from: {
+            x: 1000,
+            opacity: 0,
+            scale: 1,
+            color,
+        },
+        to: [
+            { opacity: 1, x: 0,         /**/ config: config.default, },
+            { opacity: 1, x: -400,      /**/ config: config.default, },
+            { opacity: 1, x: 0,         /**/ config: config.default, },
+            { scale: 2, color: 'white', /**/ config: { duration: 700, }, },
+            { scale: 1, },
+            { scale: 2, color: 'white', /**/ config: { duration: 400, }, },
+            { scale: 1, color, x: -50 },
+            { opacity: 0, scale: 0,     /**/ config: config.default, },
+        ],
+        config: { duration: 600, },
+    });
+
+    return (
+        <AIconGhost style={styles} className="size-8 text-indigo-100/50" />
+    );
+}
--- src/components/1-header/index.tsx
+++ src/components/1-header/index.tsx
@@ -1,33 +1,4 @@
-import { IconGhost } from "../../ui/icons";
-import { a, config, useSpring } from "@react-spring/web";
-
-const AIconGhost = a(IconGhost);
-
-function Ghost() {
-    const color = '#e0e7ff80'; //text-indigo-100/50
-    const styles = useSpring({
-        from: {
-            x: 1000,
-            opacity: 0,
-            scale: 1,
-            color,
-        },
-        to: [
-            { opacity: 1, x: 0, config: config.default, },
-            { opacity: 1, x: -400, config: config.default, },
-            { opacity: 1, x: 0, config: config.default, },
-            { scale: 2, color: 'white', config: { duration: 700, }, },
-            { scale: 1 },
-            { scale: 2, color: 'white', config: { duration: 400, }, },
-            { scale: 1, color, x: -50 },
-            { opacity: 0, scale: 0, config: config.default, },
-        ],
-        config: { duration: 600, },
-    });
-    return (
-        <AIconGhost style={styles} className="w-8 h-8 text-indigo-100/50" />
-    );
-}
+import { HeaderGhost } from "./2-header-ghost";
 
 const headerShadow = { boxShadow: '2px 2px 2px rgba(0, 0, 0, 0.2)' };
 const textShadow = { textShadow: '2px 3px #1f1e58' };
@@ -39,7 +10,8 @@ export function App_Header() {
             <div className="text-2xl font-thin text-white" style={textShadow}>
                 Password Manager Ghost Test
             </div>
-            <Ghost />
+
+            <HeaderGhost />
         </header>
     );
 }
