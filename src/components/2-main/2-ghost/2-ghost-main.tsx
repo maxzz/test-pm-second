73411a73201e08418ec0d73da0f675ac8fc5e959
Max Zakharzhevskiy <maxzz@msn.com>
2024-08-09 19:14:05 -0700

    WIP. update 2024. GhostMain code review

--- src/components/2-main/2-ghost/2-ghost-main.tsx
+++ src/components/2-main/2-ghost/2-ghost-main.tsx
@@ -1,19 +1,9 @@
 import { HTMLAttributes, useEffect, useState } from 'react';
 import { useAtom, useAtomValue } from 'jotai';
 import { a, easings, useSpring, useSpringRef } from '@react-spring/web';
-import { ghostTargetAtom, loginStartedAtom, workingAreaAtom } from '@/store';
+import { ghostTargetElementAtom, loginStartedAtom, workingAreaAtom } from '@/store';
 import { IconGhost } from '@/ui/icons';
 import { classNames } from '@/utils';
-import { ReloadButton } from './4-reload-button';
-
-const GhostAnimatedIcon = a(IconGhost);
-
-function getTargetPos(target: HTMLDivElement | null, def: { x: number; y: number; }) {
-    const pos = target?.getBoundingClientRect() || def;
-    pos.x -= 36; // Left edge of ghost SVG.
-    pos.y -= 94; // App header height (64) and top edge of ghost SVG (30).
-    return pos;
-}
 
 const animationProps = {
     from: {
@@ -40,6 +30,13 @@ const animationProps = {
     ]
 };
 
+function getTargetPosOffset(target: HTMLDivElement | null, def: { x: number; y: number; }) {
+    const pos = target?.getBoundingClientRect() || def;
+    pos.x -= 36; // Left edge of ghost SVG.
+    pos.y -= 94; // App header height (64) and top edge of ghost SVG (30).
+    return pos;
+}
+
 const xPos = (pos: number, max: number) => ({
     range: [0, 0.25, 0.5, 0.75, 1],
     output: [100, max * 0.7, max * 0.1, max * 0.5, pos + 8],
@@ -50,8 +47,10 @@ const yPos = (pos: number, max: number) => ({
     output: [100, pos / 2, pos],
 });
 
+const GhostAnimatedIcon = a(IconGhost);
+
 export function GhostMain({ className, ...rest }: HTMLAttributes<SVGSVGElement>) {
-    const ghostTarget = useAtomValue(ghostTargetAtom);
+    const ghostTargetElement = useAtomValue(ghostTargetElementAtom);
 
     const { width: areaWidth, height: areaHeight } = useAtomValue(workingAreaAtom);
     const [pos, setPos] = useState({ x: 0, y: 0 });
@@ -73,7 +72,7 @@ export function GhostMain({ className, ...rest }: HTMLAttributes<SVGSVGElement>)
     );
 
     function start() {
-        setPos(getTargetPos(ghostTarget, { x: areaWidth / 2, y: areaHeight / 2 }));
+        setPos(getTargetPosOffset(ghostTargetElement, { x: areaWidth / 2, y: areaHeight / 2 }));
 
         api.start({
             ...animationProps,
@@ -81,20 +80,20 @@ export function GhostMain({ className, ...rest }: HTMLAttributes<SVGSVGElement>)
         });
     }
 
-    return (<>
-        {/* <ReloadButton /> */}
+    if (!loginStarted) {
+        return null;
+    }
 
-        {loginStarted && (
-            <GhostAnimatedIcon
-                data-n={nmb}
-                style={{
-                    x: nmb.to(xPos(pos.x, areaWidth)),
-                    y: nmb.to(yPos(pos.y, areaHeight)),
-                    ...restAni,
-                }}
-                className={classNames("stroke-[.7] fill-slate-500 text-indigo-900", className)}
-                {...rest}
-            />
-        )}
-    </>);
+    return (
+        <GhostAnimatedIcon
+            data-n={nmb}
+            style={{
+                x: nmb.to(xPos(pos.x, areaWidth)),
+                y: nmb.to(yPos(pos.y, areaHeight)),
+                ...restAni,
+            }}
+            className={classNames("stroke-[.7] fill-slate-500 text-indigo-900", className)}
+            {...rest}
+        />
+    );
 }
