09e234a9345faa7668d3995b74cdb5c852314e7d
Max Zakharzhevskiy <maxzz@msn.com>
2024-08-10 18:22:18 -0700

    WIP. update 2024. fix reload button

--- src/components/2-main/2-ghost/4-reload-button.tsx
+++ src/components/2-main/2-ghost/4-reload-button.tsx
@@ -6,10 +6,9 @@ import { classNames } from "../../../utils/classnames";
 const buttonClasses = "self-center \
 mr-2 px-2 py-1 text-xs \
 \
-text-yellow-100 \
-border-yellow-100 hover:bg-slate-900/50 border \
-rounded \
-cursor-pointer \
+text-indigo-100 border-indigo-400 hover:bg-indigo-800 shadow-indigo-950 \
+\
+border rounded shadow cursor-pointer \
 active:scale-[.97]";
 
 export function ReloadButton({ className }: HTMLAttributes<HTMLInputElement>) {
--- src/ui/icons/index.tsx
+++ src/ui/icons/index.tsx
@@ -1,4 +1,3 @@
-export * from "./1-ghost-bubba";
 export * from "./2-ghost-dirty-deeds";
 export * from "./3-ph-user-circle-duotone";
 export * from "./4-user";
